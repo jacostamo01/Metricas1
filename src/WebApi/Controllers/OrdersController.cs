e1c59d35d66a4db181d3c9c825504f0efc76a578
jacostamo01 <jacostamo01@uninpahu.edu.co>
2026-03-28 16:25:15 -0500

    Vulnerabilidad Baja2 de mantenibilidad

--- src/WebApi/Controllers/OrdersController.cs
+++ src/WebApi/Controllers/OrdersController.cs
@@ -1,13 +1,10 @@
 using System;
 
-//Se mantiene el namespace
 namespace WebApi.Controllers
 {
     public class OrdersController 
     {
-        
         public const string DefaultMessage = "This controller does nothing. Endpoints are in Program.cs";
-
-        public string DoNothing() => DefaultMessage;
+        public static string DoNothing() => DefaultMessage;
     }
 }
