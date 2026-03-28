0ceb06c9adcfcf0d351e80237750494966019249
jacostamo01 <jacostamo01@uninpahu.edu.co>
2026-03-28 16:27:56 -0500

    Vulnerabilidad Media de mantenibilidad

--- src/WebApi/Controllers/OrdersController.cs
+++ src/WebApi/Controllers/OrdersController.cs
@@ -2,9 +2,10 @@ using System;
 
 namespace WebApi.Controllers
 {
-    public class OrdersController 
+    public static class OrdersController 
     {
         public const string DefaultMessage = "This controller does nothing. Endpoints are in Program.cs";
+
         public static string DoNothing() => DefaultMessage;
     }
 }
